c1cfb52311dcd8dae6a7c3c1b09d3cfe4e0cf18c
abi magpatoc <abimgptc@gmail.com>
2023-10-06 16:17:58 +0800

    login and logout first dev

--- Ayos_Login.php
+++ Ayos_Login.php
@@ -28,17 +28,9 @@
 <body>
 <div class="login">
     <h2>Welcome To Ayos!</h2>
-    <div class="mb-3">
-        <label for="adminEmailAdd" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="adminEmailAdd" placeholder="ayos_ba@example.com">
-    </div>
-    <div class="mb-3">
-        <label for="adminPass" class="form-label">Password</label>
-        <input type="password" class="form-control" id="adminPass">
-    </div>
-    <div>
-        <button type="submit" class="btn btn-primary mb-3">Submit</button>
-    </div>
+    <form>
+      
+    </form>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
