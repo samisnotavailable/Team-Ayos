bef8c63a1ffed464e8615df4219d72e9fc6ba0f8
abi magpatoc <abimgptc@gmail.com>
2023-10-27 09:39:43 +0800

    edit

--- Ayos_Login.php
+++ Ayos_Login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet">
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0517b0;">
         <div class="container-fluid">
           <a class="navbar-brand" href="Ayos_Home.php">Ayos!</a>
