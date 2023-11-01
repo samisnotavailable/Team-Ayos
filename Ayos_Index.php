bf0c0877b67cab285b9cce4483dd7606e38f2231
abi magpatoc <abimgptc@gmail.com>
2023-11-01 22:46:14 +0800

    deleted error

--- Ayos_Index.php
+++ Ayos_Index.php
@@ -1,10 +1,6 @@
-<?php
-session_start();
 
-if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
-  ?>
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+  <html lang="en">
     <head>   
         <title>Ayos!</title>
         <meta charset="UTF-8">
@@ -31,10 +27,5 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     </body>
-    </html>
-    <?php
-}
-else {  
-  header("Location: Ayos_Home.php");
-  exit();
-}
+</html>
+    
