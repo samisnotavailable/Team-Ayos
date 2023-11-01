129c88778b176a39c79acf2af85419dac76b2a5b
abi magpatoc <abimgptc@gmail.com>
2023-11-01 22:32:07 +0800

    navbarrrrr

--- Ayos_Index.php
+++ Ayos_Index.php
@@ -12,27 +12,21 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
-        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0517b0;">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="Ayos_Home.php">Ayos!</a>
-              <div class="topnav-right" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="Ayos_About.php">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="Ayos_Service.php">Service</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="Ayos_Login.php">Login</a>
-                  </li>
-              </div>
-            </div>
-          </nav>
+        
     </head>
     <body>
-        <h2>Home, <?php echo $_SESSION['userName']; ?></h2>
-        <a href="Ayos_Logout.php">Logout</a>
+      <nav class="navbar" style="background-color: #0517b0;">
+        <div class="navbar-brand">
+          <h1>LOGO</h1>
+        </div>
+        <ul class="nav-links">
+          <li><a href="Ayos_About.php">About</a></li>
+          <li><a href="Ayos_Service.php">Service</a></li>
+          <li><a href="Ayos_Login.php">Login</a></li>
+
+        </ul>
+      </nav>
+        <h1>This is index :D</h1>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
@@ -40,7 +34,7 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
     </html>
     <?php
 }
-else {
+else {  
   header("Location: Ayos_Home.php");
   exit();
 }
